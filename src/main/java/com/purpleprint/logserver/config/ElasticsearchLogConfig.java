22332125501801501f4d02a1297147350cbe4fd8
sanghak <yee950419@naver.com>
2022-11-25 17:57:09 +0900

    feature: #1 설정파일 추가

---
+++ src/main/java/com/purpleprint/logserver/config/ElasticsearchLogConfig.java
@@ -0,0 +1,37 @@
+package com.purpleprint.logserver.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+
+/**
+ * <pre>
+ * Class : ElasticsearchLogConfig
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-24       이상학           최초 생성
+ * </pre>
+ *
+ * @author 이상학(최초 작성자)
+ * @version 1(클래스 버전)
+ */
+@Configuration
+public class ElasticsearchLogConfig extends AbstractElasticsearchConfiguration {
+
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        final ClientConfiguration configuration = ClientConfiguration.builder()
+                .connectedTo("43.201.98.232:9200")
+                .build();
+
+        return RestClients.create(configuration).rest();
+    }
+}
