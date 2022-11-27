6c1428219fd9de57863f78d06baf2be622cca2b7
hyunjungjeon <sksznddl574@gmail.com>
2022-11-27 23:56:45 +0900

    feature : #2 행동 분석 api 호출 후 db 저장

--- src/main/java/com/purpleprint/logserver/service/LogService.java
+++ src/main/java/com/purpleprint/logserver/service/LogService.java
@@ -1,8 +1,12 @@
 package com.purpleprint.logserver.service;
 
 import com.purpleprint.logserver.dto.AnalysisDTO;
+import com.purpleprint.logserver.dto.FriendsDTO;
+import com.purpleprint.logserver.entity.Analysis;
 import com.purpleprint.logserver.model.LogModel;
+import com.purpleprint.logserver.repository.AnalysisRepository;
 import com.purpleprint.logserver.repository.LogRepository;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpEntity;
@@ -10,8 +14,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 import java.nio.charset.Charset;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -32,10 +39,12 @@ import java.util.Map;
 public class LogService {
 
     private final LogRepository logRepository;
+    private final AnalysisRepository analysisRepository;
 
     @Autowired
-    public LogService(LogRepository logRepository) {
+    public LogService(LogRepository logRepository, AnalysisRepository analysisRepository) {
         this.logRepository = logRepository;
+        this.analysisRepository = analysisRepository;
     }
 
     public LogModel addLogs(LogModel log) {
@@ -54,22 +63,80 @@ public class LogService {
         return (Page<LogModel>) logRepository.findAll();
     }
 
-    public ResponseEntity<AnalysisDTO> sendLogs(Map<String, Object> resultMap) {
+    @Transactional
+    public ResponseEntity<?> sendLogs(Map<String, Object> resultMap) {
+
+        long currentTime = new Date().getTime();
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
 
         HttpEntity<?> requestEntity = new HttpEntity<>(resultMap, headers);
 
-        String serverURL = "http://34.64.214.200:8000/";
+        System.out.println(resultMap);
+
+        String serverURL = "http://34.64.214.200:8000/user-analysis";
 
         RestTemplate restTemplate = new RestTemplate();
 
-        ResponseEntity<AnalysisDTO> response = restTemplate.getForEntity(serverURL, AnalysisDTO.class, requestEntity);
+        ResponseEntity<Map> response = restTemplate.postForEntity(serverURL, requestEntity, Map.class);
 
+        System.out.println("response : " + response.getBody());
 
-        System.out.println("response : " + response);
+        List<Map<String, Object>> analysisList = (List<Map<String, Object>> ) response.getBody().get("result");
+
+        for (Map<String, Object> analysis : analysisList) {
+
+            List<Map<String, Object>> friends = (List<Map<String, Object>>) analysis.get("friends");
+
+            Map<String, Object> bestFriends = friends.get(0);
+
+            FriendsDTO friendsDTO = new FriendsDTO();
+
+            if(!bestFriends.get("bestfriend_1").equals("") && bestFriends.get("bestfriend_1") != null) {
+                friendsDTO.setFriend1(Integer.parseInt((String) bestFriends.get("bestfriend_1")));
+                friendsDTO.setFriend1Comment(getComment(Integer.parseInt(String.valueOf(bestFriends.get("time")))));
+            }
+
+            if(!bestFriends.get("bestfriend_2").equals("") && bestFriends.get("bestfriend_2") != null) {
+                friendsDTO.setFriend2(Integer.parseInt((String) bestFriends.get("bestfriend_2")));
+                friendsDTO.setFriend2Comment(getComment(Integer.parseInt(String.valueOf(bestFriends.get("time2")))));
+            }
+
+            analysisRepository.save(new Analysis(
+                    0,
+                    friendsDTO.getFriend1(),
+                    friendsDTO.getFriend1Comment(),
+                    friendsDTO.getFriend2(),
+                    friendsDTO.getFriend2Comment(),
+                    (String) analysis.get("place"),
+                    Integer.parseInt((String) analysis.get("childid")),
+                    "N",
+                    new Date(currentTime)
+            ));
+
+        }
 
         return response;
+    }
+
+    public String getComment(int playtime) {
+
+        String playTime = "";
+
+        if((playtime / (60 * 60)) != 0) {
+            playTime = playTime + (playtime / (60 * 60)) + "시간 ";
+        }
+
+        if((playtime /60) != 0) {
+            playTime = playTime + ((playtime / 60) -  (playtime / (60 * 60) * 60)) + "분 ";
+        }
+
+        playTime = playTime + (playtime  -(playtime / 60) * 60)  + "초 ";
+
+
+        playTime = playTime + " 같이 놀았어요!";
+
+        return playTime;
     }
 }
