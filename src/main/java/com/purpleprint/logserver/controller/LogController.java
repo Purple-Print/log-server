0555b0e2957e6b2fd0b6d7be128c4f8587a71a74
sanghak <yee950419@naver.com>
2022-11-27 19:55:15 +0900

    feature: #1 매일 22시 1분에 elasticsearch 로그 기록 삭제후 파일에 저장 기능 추가, 로그 생성시 time 형식 포맷 추가

--- src/main/java/com/purpleprint/logserver/controller/LogController.java
+++ src/main/java/com/purpleprint/logserver/controller/LogController.java
@@ -1,13 +1,25 @@
 package com.purpleprint.logserver.controller;
 
+import com.purpleprint.logserver.common.responsemessage.ResponseMessage;
 import com.purpleprint.logserver.dto.AnalysisDTO;
+import com.purpleprint.logserver.dto.FileReadDTO;
 import com.purpleprint.logserver.model.LogModel;
 import com.purpleprint.logserver.service.LogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -34,8 +46,10 @@ public class LogController {
         this.logService = logService;
     }
 
-    @PostMapping("/logs")
-    public Map<String, Object> getAllLogs() {
+    //ai api 호출해서 받은 분석결과 db에 저장 로직
+    @Scheduled(cron = "0 0 22 * * *")
+    @GetMapping("/logs")
+    public ResponseEntity<AnalysisDTO> sendLogsAndGetAnalysisResult() {
 
         Page<LogModel> getLogResult = logService.getAllLogs();
 
@@ -44,37 +58,91 @@ public class LogController {
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("coorperate", logList);
 
-//        System.out.println("responsedddd : ");
+        ResponseEntity<AnalysisDTO> response = logService.sendLogs(resultMap);
 
-//        ResponseEntity<AnalysisDTO> response = logService.sendLogs(resultMap);
-//
-//        return response;
+//        boolean result = logService.setAnalysisData(response);
+
+        return response;
+    }
+
+    //생성된 모든 로그 조회
+    @PostMapping("/logs")
+    public Map<String, Object> getllLogs() {
+
+        Page<LogModel> getLogResult = logService.getAllLogs();
+
+        List<LogModel> logList = getLogResult.getContent();
+
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("coorperate", logList);
+
         return resultMap;
     }
 
-    @GetMapping("/log/{key}")
-    public LogModel getLog(@PathVariable("key") String id) {
-
-        return logService.getLog(id);
-    }
-
-    @GetMapping("/child/{id}")
-    public List<LogModel> getLogs(@PathVariable("id") int id) {
-
-        return logService.getLogs(id);
-    }
-
+    // 로그 생성 및 time 형식 포맷
     @PostMapping("/log")
     public LogModel addLogs(@RequestBody LogModel log) {
+
+        String dateFormatStringTime;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Long longTime = Long.parseLong(log.getTime());
+        Date formatDate = new Date(longTime);
+        dateFormatStringTime = dateFormat.format(formatDate);
+        log.setTime(dateFormatStringTime);
+        System.out.println(log.getTime());
 
         return logService.addLogs(log);
     }
 
-
+    //매일 22시 1분에 elasticsearch 로그 기록 삭제 후 파일에 저장, s3 스토리지에 업로드
+    @Scheduled(cron = "0 1 22 * * *")
     @DeleteMapping("/logs")
-    public void deleteLog() {
+    public ResponseEntity<?> deleteLog() throws IOException, ClassNotFoundException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
 
+        Page<LogModel> getLogResult = logService.getAllLogs();
+        List<LogModel> logList = getLogResult.getContent();
+
+        String rootLocation = System.getProperty("user.dir");
+
+        System.out.println("rootLocation : " + rootLocation);
+        FileReadDTO fileReadDTO = new FileReadDTO();
+        String filePath = rootLocation + "\\src\\main\\resources\\logs.txt";
+        System.out.println("filePath : " + filePath);
+        String arr;
+
+        //파일에 로그 정보 저장
+        for (LogModel logs : logList) {
+            fileReadDTO.setX(logs.getX());
+            fileReadDTO.setZ(logs.getZ());
+            fileReadDTO.setId(logs.getChildId());
+            fileReadDTO.setTime(logs.getTime());
+
+            arr = fileReadDTO.toString() + "\n";
+            System.out.println("log : " + arr);
+
+            try {
+                arr = arr.substring("FileReadDTO(".length(), arr.indexOf(")")) + "\n";
+                File file = new File(filePath); // File객체 생성
+                if(!file.exists()){ // 파일이 존재하지 않으면
+                    file.createNewFile(); // 신규생성
+                }
+                Files.write(Paths.get(filePath), arr.getBytes(), StandardOpenOption.APPEND);
+            }catch (IOException e) {
+                //exception handling left as an exercise for the reader
+            }
+        }
+
+        //elasticsearch에 있는 로그 정보 삭제
         logService.deleteLog();
 
+        Map<String, Object> responseMap = new HashMap<>();
+
+        return ResponseEntity
+                .ok()
+                .body(new ResponseMessage(HttpStatus.OK, "log file uploaded and elasticsearch logs deleted successfully!", responseMap));
+
     }
+
 }
