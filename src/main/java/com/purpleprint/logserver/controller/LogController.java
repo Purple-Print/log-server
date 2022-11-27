70b02f6733ed15e3a396b2ff06704f1f75c5e166
sanghak <yee950419@naver.com>
2022-11-28 00:48:46 +0900

    feature: #1  LogController 코드 정리

--- src/main/java/com/purpleprint/logserver/controller/LogController.java
+++ src/main/java/com/purpleprint/logserver/controller/LogController.java
@@ -15,7 +15,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
